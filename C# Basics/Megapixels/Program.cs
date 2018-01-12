442ed896f274e45b33a90ca1f80adfd395088203
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-12 19:11:28 +0200

    Megapixels

---
+++ C# Basics/Megapixels/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Megapixels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            double result = ((double)width * height) / 1000000;
+            result = Math.Round(result, 1);
+
+            Console.WriteLine($"{width}x{height} => {result}MP"); 
+           
+        }
+    }
+}
