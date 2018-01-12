4f7d96dc8017f2564b80813daa4402c4f5d17baa
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-12 17:58:43 +0200

    SumOfOddNumbers

---
+++ C# Conditional Statements and Loops-lab/SumOfOddNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int result = 0;
+
+            for (int i = 1; i < num * 2; i++)
+            {
+
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                    result += i;
+                }
+            }
+            Console.WriteLine($"Sum: {result}");
+        }
+    }
+}
