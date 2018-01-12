e35706bfa31a67e210134135eb560a6f25a2274f
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-12 18:01:29 +0200

    PassedOrFailed

---
+++ C# Conditional Statements and Loops-lab/PassedOrFailed/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassedOrFailed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            if (grade >= 3.00)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
