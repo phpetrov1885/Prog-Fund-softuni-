50273fd0b3db7664095ad21d7f0fdb83a930d851
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-12 18:02:24 +0200

    Passed

---
+++ C# Conditional Statements and Loops-lab/OddNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                if (numb % 2 == 0)
+                {
+                    Console.WriteLine("Please write an odd number.");
+                    numb = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(numb)}");
+                    break;
+                }
+            }
+        }
+    }
+}
