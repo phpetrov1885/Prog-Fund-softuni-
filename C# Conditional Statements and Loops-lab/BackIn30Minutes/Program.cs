d5b741259a9ef0375df871340d6fbd59c9ac1b97
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-12 18:12:04 +0200

    BackInThirtyMinutes

---
+++ C# Conditional Statements and Loops-lab/BackIn30Minutes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            if (minutes > 59)
+            {
+                hours++;
+                minutes = minutes % 60;
+            }
+
+            if (hours > 23)
+            {
+                hours -= 24;
+            }
+
+            Console.WriteLine($"{hours}:{minutes:D2}");
+        }
+    }
+}
