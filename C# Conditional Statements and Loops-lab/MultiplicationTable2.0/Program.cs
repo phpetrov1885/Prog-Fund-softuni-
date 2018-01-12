6455985a54f0e78a93a0df2c17c77d4e8fe7c05b
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-12 18:03:21 +0200

    MultiplicationTable2.0

---
+++ C# Conditional Statements and Loops-lab/MultiplicationTable2.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int result = 0;
+
+
+            for (int i = secondNum; i <= 10; i++)
+            {
+                result = firstNum * i;
+                Console.WriteLine($"{firstNum} X {i} = {result}");
+            }
+            if (secondNum > 10)
+            {
+                result = firstNum * secondNum;
+                Console.WriteLine($"{firstNum} X {secondNum} = {result}");
+            }
+        }
+    }
+}
