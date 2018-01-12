8a8b5380368d639a63b908557d2604311134ee05
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-12 19:10:21 +0200

    BPMCounter

---
+++ C# Basics/BPMCounter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BPMCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int beatsPerMinutes = int.Parse(Console.ReadLine());
+            int numberOfBeats = int.Parse(Console.ReadLine());
+           
+            double bars = Math.Round(((double)numberOfBeats / 4), 1);
+
+            double beatsPerSecond = (double)beatsPerMinutes / 60;
+            double seconds = (int)numberOfBeats / beatsPerSecond;
+
+            var time = TimeSpan.FromSeconds(seconds);
+
+            Console.WriteLine($"{bars} bars - {time.Minutes}m {time.Seconds}s");
+        }
+    }
+}
