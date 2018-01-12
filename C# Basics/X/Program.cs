074c61297aeb71ee5c149eb22abb5cbd685f3710
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-12 19:13:22 +0200

    X

---
+++ C# Basics/X/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= numb - 1; i++)
+            {
+                for (int j = 0; j <= numb; j++)
+                {
+                    if ((i == numb - j - 1) ||
+                        (i == j))
+                    {
+                        Console.Write("x");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
