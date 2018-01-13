d0e4c8e18194094eec6283a491072b75360b1e29
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:23:00 +0200

    PracticeCharsAndStrings

---
+++ Data Types and Variables - Exercises/PracticeCharsAndStrings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCharsAndStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstInput = "Software University";
+            char secondInput = 'B';
+            char thirdInput = 'y';
+            char fourtInput = 'e';
+            string fifthInput = "I love programming";
+
+            Console.WriteLine(firstInput);
+            Console.WriteLine(secondInput);
+            Console.WriteLine(thirdInput);
+            Console.WriteLine(fourtInput);
+            Console.WriteLine(fifthInput);
+        }
+    }
+}
