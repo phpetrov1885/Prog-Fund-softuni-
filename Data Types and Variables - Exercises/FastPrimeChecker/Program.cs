1d24cbabfe3d885e74e18fdee120d9345c7bc43f
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:22:06 +0200

    FastPrimeChecker

---
+++ Data Types and Variables - Exercises/FastPrimeChecker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = int.Parse(Console.ReadLine());
+            for (int col = 2; col <= numb; col++)
+            {
+                bool isPrime = true;
+                for (int row = 2; row <= Math.Sqrt(col); row++)
+                {
+                    if (col % row == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{col} -> {isPrime}");
+            }
+        }
+    }
+}
