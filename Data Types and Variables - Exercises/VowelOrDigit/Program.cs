11e2efbb23a8cba851335b23cb70a9a0517c8a8b
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:26:51 +0200

    VowelOrDigit

---
+++ Data Types and Variables - Exercises/VowelOrDigit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VowelOrDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+
+            if (char.IsDigit(input))
+            {
+                Console.WriteLine("digit");
+            }
+            else
+            {
+                if (input == 'a' || input == 'i'
+                    || input == 'e' || input == 'o'
+                    || input == 'u')
+                {
+                    Console.WriteLine("vowel");
+                }
+                else
+                {
+                    Console.WriteLine("other");
+                }
+            }
+        }
+    }
+}
