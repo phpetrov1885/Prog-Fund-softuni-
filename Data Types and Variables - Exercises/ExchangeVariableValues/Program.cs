9d27f7c6ba72670b4eb5b7ecf1c62efe544b85f5
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:21:35 +0200

    ExchangeVariableValues

---
+++ Data Types and Variables - Exercises/ExchangeVariableValues/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExchangeVariableValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Before:");
+            Console.WriteLine($"a = {firstNum}");
+            Console.WriteLine($"b = {secondNum}");
+
+            int temp = 0;
+            temp = firstNum;
+            firstNum = secondNum;
+            secondNum = temp;
+
+            Console.WriteLine("After:");
+            Console.WriteLine($"a = {firstNum}");
+            Console.WriteLine($"b = {secondNum}");
+        }
+    }
+}
