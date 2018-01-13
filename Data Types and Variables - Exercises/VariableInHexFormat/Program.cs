68c2de24c7f75a122a62119bdb5ee9786a314d6f
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:26:26 +0200

    VariableInHexFormat

---
+++ Data Types and Variables - Exercises/VariableInHexFormat/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableInHexFormat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Console.WriteLine(Convert.ToInt32(input, 16));
+        }
+    }
+}
