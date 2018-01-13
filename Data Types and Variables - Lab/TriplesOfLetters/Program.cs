f7c71364b8d1ea0c0c248f01f10e194a5563b219
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:12:55 +0200

    TriplesOfLetters

---
+++ Data Types and Variables - Lab/TriplesOfLetters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriplesOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numb = int.Parse(Console.ReadLine());
+            
+            for (int i = 97; i < 97 + numb; i++)
+            {
+                for (int j = 97; j < 97 + numb; j++)
+                {
+                    for (int k = 97; k < 97 + numb; k++)
+                    {
+                        Console.WriteLine($"{(char)i}{(char)j}{(char)k}");
+                    }
+                }
+            }
+        }
+    }
+}
