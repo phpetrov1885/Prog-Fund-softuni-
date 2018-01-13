bc7bee4c528a3c3e7ebf3b034b34c74f8d9145c2
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:24:27 +0200

    PrintPartOfASCIITable

---
+++ Data Types and Variables - Exercises/PrintPartOfASCIITable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintPartOfASCIITable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumb = int.Parse(Console.ReadLine());
+            int secondNumb = int.Parse(Console.ReadLine());
+
+            for (int i = firstNumb; i <= secondNumb; i++)
+            {
+                char result = (char)i;
+                Console.Write(result + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
