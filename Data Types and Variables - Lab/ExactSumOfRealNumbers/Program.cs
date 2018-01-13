9e69c6f33685343da9aac69184ce3897594b0048
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:10:32 +0200

    ExactSumOfRealNumbers

---
+++ Data Types and Variables - Lab/ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExactSumOfRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numCount = int.Parse(Console.ReadLine());
+            decimal sum = 0;
+
+            for (int i = 0; i < numCount; i++)
+            {
+                decimal digit = decimal.Parse(Console.ReadLine());
+                sum += digit;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
