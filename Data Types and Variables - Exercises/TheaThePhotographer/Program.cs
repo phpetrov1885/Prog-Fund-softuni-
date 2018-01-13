82cd062cec46a288fb7d52178edc6079d6ee1419
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:26:04 +0200

    TheaThePhotographer

---
+++ Data Types and Variables - Exercises/TheaThePhotographer/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheaThePhotographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long allPictures = long.Parse(Console.ReadLine());
+            int filterTime = int.Parse(Console.ReadLine());
+            int percentageGoodPic = int.Parse(Console.ReadLine());
+            int uploadTime = int.Parse(Console.ReadLine());
+
+
+            long usefulPictures = (long)Math.Ceiling(allPictures * (percentageGoodPic / 100.0));
+
+
+            long uploadingAllPic = (long)allPictures * filterTime;
+            long uploadingUsefulPic = (long)usefulPictures * uploadTime;
+            long allSecondsNeeded = uploadingUsefulPic + uploadingAllPic;
+
+            var time = TimeSpan.FromSeconds(allSecondsNeeded);
+
+            Console.WriteLine($"{time.Days}:{time.Hours:d2}:{time.Minutes:d2}:{time.Seconds:d2}");
+        }
+    }
+}
