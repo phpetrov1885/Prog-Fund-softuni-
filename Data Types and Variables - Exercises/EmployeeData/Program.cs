f3b65c4ad0956653267e3aa01f0eef838d96c5cb
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:21:10 +0200

    EmployeeData

---
+++ Data Types and Variables - Exercises/EmployeeData/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = "Amanda";
+            string lastName = "Jonson";
+            byte age = 27;
+            char gender = 'f';
+            long personalIDNumb = 8306112507;
+            long employeeNumb = 27563571;
+
+            Console.WriteLine($"First name: {firstName}");
+            Console.WriteLine($"Last name: {lastName}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Gender: {gender}");
+            Console.WriteLine($"Personal ID: {personalIDNumb}");
+            Console.WriteLine($"Unique Employee number: {employeeNumb}");
+        }
+    }
+}
