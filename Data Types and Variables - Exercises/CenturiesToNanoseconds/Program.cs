2c0462504dd35ed16a5d1957af4c184d94f8787d
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:16:40 +0200

    CenturiesToNanoseconds

---
+++ Data Types and Variables - Exercises/CenturiesToNanoseconds/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CenturiesToNanoseconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int century = int.Parse(Console.ReadLine());
+
+            int years = century * 100;
+            decimal days = years * 365.2422m;
+            days = Math.Floor(days);
+            long hours = (int)days * 24;
+            long minutes = hours * 60;
+            long seconds = minutes * 60;
+            long milliSeconds = seconds * 1000;
+            long microSeconds = milliSeconds * 1000;
+
+            BigInteger nanoSeconds = new BigInteger(microSeconds);
+            nanoSeconds = nanoSeconds * 1000;
+
+            Console.WriteLine($"{century} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = " +
+                              $"{seconds} seconds = {milliSeconds} milliseconds" +
+                              $" = {microSeconds} microseconds = {nanoSeconds} nanoseconds");
+        }
+    }
+}
---
+++ Data Types and Variables - Exercises/CenturiesToNanoseconds/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CenturiesToNanoseconds")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CenturiesToNanoseconds")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c1aed859-2e5b-4fa4-8897-b7e5f8ee240f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
