e77a4dd08d7c19b00edd03cafaae8c8513b8c4d9
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:09:41 +0200

    Elevator

---
+++ Data Types and Variables - Lab/Elevator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int personCount = int.Parse(Console.ReadLine());
+            int elevatorCapacity = int.Parse(Console.ReadLine());
+            int course = 0;
+
+            while (true)
+            {
+                course++;
+                personCount -= elevatorCapacity;
+                if (personCount <= 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(course);
+        }
+    }
+}
