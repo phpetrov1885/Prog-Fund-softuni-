80c3384aa3d7687ac19229c8ac742ec87c6133f6
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:24:52 +0200

    RectangleProperties

---
+++ Data Types and Variables - Exercises/RectangleProperties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double widht = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double rectanglePerimeter = (widht + height) * 2;
+            double rectangleArea = widht * height;
+            double sum = (widht * widht) + (height * height);
+            double rectangleDiagonal = Math.Sqrt(sum);
+
+            Console.WriteLine(rectanglePerimeter);
+            Console.WriteLine(rectangleArea);
+            Console.WriteLine(rectangleDiagonal);
+        }
+    }
+}
