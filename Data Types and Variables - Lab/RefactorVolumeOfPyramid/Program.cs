36bb44956003b5a7ce43c64b163223fb94da912a
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:12:04 +0200

    RefactorVolumeOfPyramid

---
+++ Data Types and Variables - Lab/RefactorVolumeOfPyramid/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorVolumeOfPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            double lenght = double.Parse(Console.ReadLine());
+
+            Console.Write("Width: ");
+            double widht = double.Parse(Console.ReadLine());
+
+            Console.Write("Height: ");
+            double height = double.Parse(Console.ReadLine());
+
+            double area = 0;
+            area = (lenght * widht * height) / 3;
+
+            Console.WriteLine("Pyramid Volume: {0:F2}", area);
+        }
+    }
+}
