da90bc11ac16a6b9abc18382a86876276ff16de7
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:17:06 +0200

    ComparingFloats

---
+++ Data Types and Variables - Exercises/ComparingFloats/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparingFloats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumb = double.Parse(Console.ReadLine());
+            double secondNumb = double.Parse(Console.ReadLine());
+            bool isEquals = false;
+            double eps = 0.000001;
+
+            if (Math.Abs(firstNumb) - Math.Abs(secondNumb) > eps ||
+                Math.Abs(firstNumb) - Math.Abs(secondNumb) > eps)
+            {
+                isEquals = false;
+            }
+            else
+            {
+                isEquals = true;
+            }
+            Console.WriteLine(isEquals);
+        }
+    }
+}
