6eae2d6e0ca9552159f8ba107909ceb4b8c44d29
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:16:07 +0200

    BooleanVariable

---
+++ Data Types and Variables - Exercises/BooleanVariable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string result = string.Empty;
+
+            if (input == "True")
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
