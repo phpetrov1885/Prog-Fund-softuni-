9b4e7fac4e96b0aeb40c4d77f884af23f77567fa
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:09:09 +0200

    CircleArea

---
+++ Data Types and Variables - Lab/CircleArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius = double.Parse(Console.ReadLine());
+
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine($"{area:f12}");
+        }
+    }
+}
