133370dc5a89a6476474773cdfdef526b3df5696
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:22:29 +0200

    IntegerToHexAndBinary

---
+++ Data Types and Variables - Exercises/IntegerToHexAndBinary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegerToHexAndBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            string binary = Convert.ToString(input, 2);
+            string hexDecimal = input.ToString("X");
+
+            Console.WriteLine(hexDecimal);
+            Console.WriteLine(binary);
+        }
+    }
+}
