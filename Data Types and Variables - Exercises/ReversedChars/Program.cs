3c740cb5673027ff49f50c5e2f54358dc2dd0b3f
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:25:16 +0200

    ReversedChars

---
+++ Data Types and Variables - Exercises/ReversedChars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char thirdLetter = char.Parse(Console.ReadLine());
+
+            string result = $"{thirdLetter}{secondLetter}{firstLetter}";
+            Console.WriteLine(result);
+        }
+    }
+}
