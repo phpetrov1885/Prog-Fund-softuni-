536cb2f92080f309c1d9f852a00ab16871c1d51b
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-13 19:25:42 +0200

    StringsAndObjects

---
+++ Data Types and Variables - Exercises/StringsAndObjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = "Hello";
+            string secondWord = "World";
+            object merge = $"{firstWord} {secondWord}";
+
+            string result = (string)merge;
+
+            Console.WriteLine(result);
+        }
+    }
+}
