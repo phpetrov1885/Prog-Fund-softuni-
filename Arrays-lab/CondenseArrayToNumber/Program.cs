49dc59d4f3f555f01d66b2be7a7581a03373642e
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:33:44 +0200

    CondenseArrayToNumber

---
+++ Arrays-lab/CondenseArrayToNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            if (inputArray.Length == 1)
+            {
+                Console.WriteLine($"{inputArray[0]} is already condensed to number");
+            }
+            else
+            {
+                int[] condensedArr = new int[inputArray.Length - 1];
+                int count = 0;
+                while (count != inputArray.Length - 1)
+                {
+                    count++;
+                    for (int i = 0; i < condensedArr.Length; i++)
+                    {
+                        condensedArr[i] = inputArray[i] + inputArray[i + 1];
+                        inputArray[i] = condensedArr[i];
+                    }
+
+                }
+                Console.WriteLine(condensedArr[0]);
+            }
+        }
+    }
+}
