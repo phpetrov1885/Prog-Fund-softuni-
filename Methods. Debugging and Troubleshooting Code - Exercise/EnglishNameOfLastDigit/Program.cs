0b86cfd862f0234e33d2cda7ba1c3a9a5b2842c0
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:24:57 +0200

    EnglishNameOfLastDigit

---
+++ Methods. Debugging and Troubleshooting Code - Exercise/EnglishNameOfLastDigit/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishNameOfLastDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long number = long.Parse(Console.ReadLine());
+            number = Math.Abs(number);
+
+            long lastDigit = GetLastDigit(number);
+
+            switch (lastDigit)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+                case 4:
+                    Console.WriteLine("four");
+                    break;
+                case 5:
+                    Console.WriteLine("five");
+                    break;
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+                case 7:
+                    Console.WriteLine("seven");
+                    break;
+                case 8:
+                    Console.WriteLine("eight");
+                    break;
+                case 9:
+                    Console.WriteLine("nine");
+                    break;
+                case 0:
+                    Console.WriteLine("zero");
+                    break;
+            }
+        }
+
+        private static long GetLastDigit(long number)
+        {
+            long result = number % 10;
+            return result;
+        }
+    }
+}
