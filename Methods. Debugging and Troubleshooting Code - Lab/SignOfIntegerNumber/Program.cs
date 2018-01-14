b05b0111bd549c513b17fa9fb861fd7e73cfa80b
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:20:00 +0200

    SignOfIntegerNumber

---
+++ Methods. Debugging and Troubleshooting Code - Lab/SignOfIntegerNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignOfIntegerNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            PrintSign(number);
+        }
+
+        static void PrintSign(int number)
+        {
+            if (number > 0)
+            {
+                Console.WriteLine("The number {0} is positive.", number);
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine("The number {0} is negative.", number);
+            }
+            else
+            {
+                Console.WriteLine("The number {0} is zero.", number);
+            }
+        }
+    }
+}
