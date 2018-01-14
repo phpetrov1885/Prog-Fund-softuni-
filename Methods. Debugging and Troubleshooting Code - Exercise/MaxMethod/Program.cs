cf6a53f2a19adfefa63e6b7016e6fef75b783657
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:28:03 +0200

    MaxMethod

---
+++ Methods. Debugging and Troubleshooting Code - Exercise/MaxMethod/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int maxNumber = GetMaxNumber(firstNumber, secondNumber, thirdNumber);
+            Console.WriteLine(maxNumber);
+        }
+
+        static int GetMaxNumber(int firstNumber, int secondsNumber, int thirdNumber)
+        {
+            int resultMaxNum = Math.Max(firstNumber, Math.Max(secondsNumber, thirdNumber));
+            return resultMaxNum;
+        }
+    }
+}
