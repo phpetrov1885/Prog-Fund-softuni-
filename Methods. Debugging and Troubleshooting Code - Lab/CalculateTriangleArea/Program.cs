c89d61cfc7a7d33f56d046099bd2a40452a6b9e8
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:16:53 +0200

    CalculateTriangleArea

---
+++ Methods. Debugging and Troubleshooting Code - Lab/CalculateTriangleArea/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateTriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double triangleWidht = double.Parse(Console.ReadLine());
+            double triangleHeight = double.Parse(Console.ReadLine());
+
+            double triangleArea = GetTriangleArea(triangleWidht, triangleHeight);
+
+            Console.WriteLine($"{triangleArea}");
+        }
+
+
+        static double GetTriangleArea(double triangleWidht, double triangleHeight)
+        {
+            double result = triangleWidht * triangleHeight / 2;
+            return result;
+        }
+    }
+}
