18e3a97a1e1eaaf8606f5cab9b909430b0049211
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:36:23 +0200

    SumArrays

---
+++ Arrays-lab/SumArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstArr = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] secondArr = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int arrMaxLenght = Math.Max(firstArr.Length, secondArr.Length);
+            int [] resultArr=new int[arrMaxLenght];
+
+            for (int i = 0; i < resultArr.Length; i++)
+            {
+                int sum = firstArr[i % firstArr.Length] + secondArr[i % secondArr.Length];
+                resultArr[i] = sum;
+            }
+            Console.WriteLine(string.Join(" ",resultArr));
+        }
+    }
+}
