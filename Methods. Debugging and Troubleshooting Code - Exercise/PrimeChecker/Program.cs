581e494ee7e2bb51cac515ac8d8e11fdc89567cd
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:29:27 +0200

    PrimeChecker

---
+++ Methods. Debugging and Troubleshooting Code - Exercise/PrimeChecker/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            n = Math.Abs(n);
+
+            Console.WriteLine(IfTheNumberIsPRime(n));
+        }
+
+        private static bool IfTheNumberIsPRime(long n)
+        {
+
+            if (n == 1 || n == 0)
+            {
+                return false;
+            }
+            if (n == 2)
+            {
+                return true;
+            }
+            for (long i = 2; i <= Math.Ceiling(Math.Sqrt(n)); i++)
+            {
+                if (n % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
+
