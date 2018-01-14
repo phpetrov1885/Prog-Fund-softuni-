23109e4c2bdd204f80b6d06679f4763e1737e93c
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:19:14 +0200

    MultiplyEvensByOdds

---
+++ Methods. Debugging and Troubleshooting Code - Lab/MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            number = Math.Abs(number);
+            int result = GetMultipleOfEvensAndOdds(number);
+
+            Console.WriteLine(result);
+        }
+
+        static int GetMultipleOfEvensAndOdds(int number)
+        {
+            int oddSum = GetSumOfOddNums(number);
+            int evenSum = GetSumOfEvenNums(number);
+            return oddSum * evenSum;
+        }
+
+        static int GetSumOfOddNums(int number)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                int lastNum = number % 10;
+                if (lastNum % 2 != 0)
+                {
+                    sum += lastNum;
+                }
+
+                number /= 10;
+            }
+            return sum;
+        }
+
+        static int GetSumOfEvenNums(int number)
+        {
+            int sum = 0;
+
+            while (number > 0)
+            {
+                int lastNum = number % 10;
+                if (lastNum % 2 == 0)
+                {
+                    sum += lastNum;
+                }
+
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
