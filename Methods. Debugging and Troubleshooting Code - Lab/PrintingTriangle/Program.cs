02b5923948512a7946a5a33c2b4335859b0614a3
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:19:35 +0200

    PrintingTriangle

---
+++ Methods. Debugging and Troubleshooting Code - Lab/PrintingTriangle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            UpperSide(number);
+            FooterSide(number);
+        }
+
+        static void UpperSide(int number)
+        {
+            for (int i = 1; i <= number; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        static void FooterSide(int number)
+        {
+            for (int i = number - 1; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
