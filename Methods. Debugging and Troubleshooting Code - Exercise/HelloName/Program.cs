d025d16d7e8059b0d941fd2559b9be37b2447f31
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:26:45 +0200

    HelloName

---
+++ Methods. Debugging and Troubleshooting Code - Exercise/HelloName/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = Console.ReadLine();
+
+            PrintPersonName(name);
+
+        }
+
+        static void PrintPersonName(string name)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
