6c7c80fca5f65fe3a8d646fcea8177bd9f7fd96a
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:26:21 +0200

    GeometryCalculator

---
+++ Methods. Debugging and Troubleshooting Code - Exercise/GeometryCalculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figureType = Console.ReadLine();
+            double areaResult = 0;
+            if (figureType == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                areaResult = GetTriangleArea(side, height);
+            }
+            else if (figureType == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                areaResult = GetSquareArea(side);
+            }
+            else if (figureType == "rectangle")
+            {
+                double widht = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                areaResult = GetRectangleArea(widht, height);
+            }
+            else if (figureType == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                areaResult = GetCircleArea(radius);
+            }
+            Console.WriteLine($"{areaResult:f2}");
+        }
+
+        private static double GetCircleArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        private static double GetRectangleArea(double widht, double height)
+        {
+            return widht * height;
+        }
+
+        private static double GetSquareArea(double side)
+        {
+            return side * side;
+        }
+
+        private static double GetTriangleArea(double side, double height)
+        {
+            return (side * height) / 2;
+        }
+    }
+}
+
