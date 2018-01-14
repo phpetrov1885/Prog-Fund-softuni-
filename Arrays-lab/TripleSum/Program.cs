727d215ef5de9117b9ffcc9c4a50a4e0b9f3cb31
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:36:44 +0200

    TripleSum

---
+++ Arrays-lab/TripleSum/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TripleSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            bool isSummed = false;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    var sum = inputArray[i] + inputArray[j];
+                    if (inputArray.Contains(sum))
+                    {
+                        Console.WriteLine($"{inputArray[i]} + {inputArray[j]} == {sum}");
+                        isSummed = true;
+                    }
+                }
+            }
+            if (!isSummed)
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
