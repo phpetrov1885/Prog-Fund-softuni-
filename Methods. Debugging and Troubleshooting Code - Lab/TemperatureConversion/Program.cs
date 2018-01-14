98dd08882e45e44a9a26019f7a21cd52cff4768b
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:20:22 +0200

    TemperatureConversion

---
+++ Methods. Debugging and Troubleshooting Code - Lab/TemperatureConversion/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemperatureConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperatureFahrenheit = double.Parse(Console.ReadLine());
+            double temperatureCelsius = FromFahrenheitToCelsius(temperatureFahrenheit);
+            Console.WriteLine($"{temperatureCelsius:f2}");
+        }
+
+        static double FromFahrenheitToCelsius(double temperatureFahrenheit)
+        {
+            double result = (temperatureFahrenheit - 32) * 5 / 9;
+            return result;
+        }
+    }
+}
