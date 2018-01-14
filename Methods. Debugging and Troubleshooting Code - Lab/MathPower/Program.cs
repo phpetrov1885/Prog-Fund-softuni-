3c75d2d3f2c64d76eafcb59c7e92f3346237bc54
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:18:48 +0200

    MathPower

---
+++ Methods. Debugging and Troubleshooting Code - Lab/MathPower/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum = double.Parse(Console.ReadLine());
+            double secondNum = double.Parse(Console.ReadLine());
+
+            double numPower = GetTNumberPower(firstNum, secondNum);
+
+            Console.WriteLine($"{numPower}");
+        }
+
+
+        static double GetTNumberPower(double firstNum, double secondNum)
+        {
+            return Math.Pow(firstNum, secondNum);
+        }
+    }
+}
