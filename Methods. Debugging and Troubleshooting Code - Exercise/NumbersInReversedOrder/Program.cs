bb25e96acf54f1837264eb4cff843d770f6ad92c
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:29:03 +0200

    Add files via upload

---
+++ Methods. Debugging and Troubleshooting Code - Exercise/NumbersInReversedOrder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersInReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            PrintDigitReversed(number);
+        }
+
+        private static void PrintDigitReversed(string number)
+        {
+            char[] charArray = number.ToArray();
+            Console.WriteLine(string.Join("", charArray.Reverse()));
+        }
+    }
+}
