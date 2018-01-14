bda1d1d68d4af5096b839b3bd2be417e13f16606
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:16:10 +0200

    BlankReceipt

---
+++ Methods. Debugging and Troubleshooting Code - Lab/BlankReceipt/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlankReceipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReceiptHeader();
+            ReceiptBody();
+            ReceiptFooter();
+        }
+
+        static void ReceiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+
+        static void ReceiptBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+        static void ReceiptFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+    }
+}
