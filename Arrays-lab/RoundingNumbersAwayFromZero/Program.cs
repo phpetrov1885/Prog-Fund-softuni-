836c450d92f2693e2c62e99cad2d8763ce9cfe63
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:36:00 +0200

    RoundingNumbersAwayFromZero

---
+++ Arrays-lab/RoundingNumbersAwayFromZero/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoundingNumbersAwayFromZero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] inputArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                Console.WriteLine($"{inputArray[i]} => {Math.Round(inputArray[i],MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
