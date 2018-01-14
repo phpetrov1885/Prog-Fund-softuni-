074fa90782742650951eba25c31a6ff4c518c999
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:35:38 +0200

    ReverseArrayOfStrings

---
+++ Arrays-lab/ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputArr = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(string.Join(" " ,inputArr.Reverse()));
+        }
+    }
+}
