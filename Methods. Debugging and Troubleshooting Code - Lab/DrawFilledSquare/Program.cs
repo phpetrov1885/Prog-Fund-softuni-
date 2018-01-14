610a80b1c6d5a482cf2491baf2b40178479196eb
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-14 18:17:21 +0200

    DrawFilledSquare

---
+++ Methods. Debugging and Troubleshooting Code - Lab/DrawFilledSquare/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawFilledSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            SquareHeader(number);
+            SquareBody(number);
+            SquareFooter(number);
+        }
+
+        static void SquareHeader(int number)
+        {
+
+            Console.WriteLine(new string('-', 2 * number));
+
+        }
+
+        static void SquareBody(int number)
+        {
+            for (int i = 0; i < number - 2; i++)
+            {
+                Console.Write('-');
+                for (int j = 1; j < number; j++)
+                {
+                    Console.Write("\\/");
+                }
+                Console.WriteLine('-');
+            }
+        }
+
+        static void SquareFooter(int number)
+        {
+            Console.WriteLine(new string('-', 2 * number));
+        }
+    }
+}
