5944f146fcd67398390d90576a7be4373785a615
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 14:41:55 +0200

    CompareCharArrays

---
+++ Arrays-Exercises/CompareCharArrays/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] firstArr = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+            char[] secondArr = Console.ReadLine().Split(' ').Select(char.Parse).ToArray();
+
+
+            char firstArrSum = (char)firstArr.Sum(f => f);
+            char secondArrSum = (char)secondArr.Sum(s => s);
+
+
+            if (firstArrSum < secondArrSum)
+            {
+                Console.WriteLine(string.Join("", firstArr));
+                Console.WriteLine(string.Join("", secondArr));
+            }
+            else
+            {
+                Console.WriteLine(string.Join("", secondArr));
+                Console.WriteLine(string.Join("", firstArr));
+            }
+        }
+
+    }
+
+}
+
