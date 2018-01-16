5b35e7931b95b964eeedcae9b532d69a9473d77a
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 14:43:31 +0200

    IndexOfLetters

---
+++ Arrays-Exercises/IndexOfLetters/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _06.Batteries
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class IndexOfLetters
+    {
+        public static void Main()
+        {
+
+            char[] alphabetArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToLower().ToCharArray();
+	    char[] inputArray = Console.ReadLine().ToCharArray();
+
+            char matchingElement = ' ';
+            int indexOfElement = 0;
+
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < alphabetArray.Length; j++)
+                {
+                    if (inputArray[i] == alphabetArray[j])
+                    {
+                        matchingElement = alphabetArray[j];
+                        indexOfElement = j;
+                    }
+                }
+                Console.WriteLine($"{matchingElement} -> {indexOfElement}");
+            }
+        }
+    }
+}
