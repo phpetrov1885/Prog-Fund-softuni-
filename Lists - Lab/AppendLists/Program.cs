bb8d6e1d9b3c736d351232c15c8f7d1ec39259b1
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 18:14:35 +0200

    AppendLists

---
+++ Lists - Lab/AppendLists/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppendLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputNumbs = Console.ReadLine().Split('|');
+
+
+            List<int> result = new List<int>();
+
+            for (int i = inputNumbs.Length - 1; i >= 0; i--)
+            {
+                List<int> leffSide = inputNumbs[i]
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+                result.AddRange(leffSide);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
