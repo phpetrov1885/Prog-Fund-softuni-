d9adaa6e0e5bc71f151a2fa64a0bcc924b5a10fa
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 14:44:25 +0200

    MaxSequenceEqualElements

---
+++ Arrays-Exercises/MaxSequenceEqualElements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSequenceEqualElements
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = 1;
+            int maxCount = 1;
+            int macth=inputArray[0];
+         
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                if (inputArray[i] == inputArray[i + 1])
+                {
+                    count++;
+		    if (count > maxCount)
+                    {
+                        maxCount = count;
+                         macth = inputArray[i];
+                    }
+		}
+                else
+                {
+                    count = 1;
+                }
+            }
+
+            for (int i = 0; i < maxCount; i++)
+            {
+                Console.Write($"{macth} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
