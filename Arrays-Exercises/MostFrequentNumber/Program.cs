1171c7eef6344b5ead8d44672eda84ddc8fc4b82
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 14:57:25 +0200

    MostFrequentNumber

---
+++ Arrays-Exercises/MostFrequentNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MostFrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            int tempCount = 1;
+            int maxCount = 1;
+            int count = 1;
+            int temp = 0;
+            int match = 0;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                temp = inputArray[i];
+                tempCount = 0;
+
+                for (int j = 1; j < inputArray.Length; j++)
+                {
+                    if (temp==inputArray[j])
+                    {
+                        tempCount++;
+                    }
+                }
+                if (tempCount>count)
+                {
+                    match = temp;
+                    count = tempCount;
+                }
+            }
+            Console.WriteLine(match);
+        }
+    }
+}
