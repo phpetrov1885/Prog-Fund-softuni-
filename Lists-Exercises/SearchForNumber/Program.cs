c65c47fd41e558c770811c3e9917928082c2236b
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 18:21:45 +0200

    SearchForNumber

---
+++ Lists-Exercises/SearchForNumber/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchForNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> inputNumbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int[] manipulations = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = manipulations[0];
+
+            inputNumbers = inputNumbers.Take(count).ToList();
+            inputNumbers.RemoveRange(0, manipulations[1]);
+            int numberToFind = manipulations[2];
+
+            if (inputNumbers.Contains(numberToFind))
+            {
+                Console.WriteLine("YES!");
+            }
+            else
+            {
+                Console.WriteLine("NO!");
+            }
+        }
+    }
+}
+
