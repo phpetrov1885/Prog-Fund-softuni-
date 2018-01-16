64ea1118d21243719ed9be6a258235bb3ed05134
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 14:44:53 +0200

    MaxSequenceIncreasingElements

---
+++ Arrays-Exercises/MaxSequenceIncreasingElements/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSequenceIncreasingElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int count = 1;
+            int maxCount = 1;
+            int element = inputArray[0];
+            int seqStart = 0;
+            int lenght = 1;
+           
+            
+            for (int i =1; i < inputArray.Length; i++)
+            {
+                if (inputArray[i]>inputArray[i-1])
+                {
+                    count++;
+
+                    if (count>maxCount)
+                    {
+                        maxCount = count;
+                        lenght = seqStart;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                    seqStart = i;
+                }
+            }
+
+            for (int i = lenght; i < lenght+maxCount; i++)
+            {
+                Console.Write(inputArray[i]+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
