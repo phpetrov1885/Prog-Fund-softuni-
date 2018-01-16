cd9ce523a43dfb8176e152d424d7959c5639003d
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 18:22:11 +0200

    SumReversedNumbers

---
+++ Lists-Exercises/SumReversedNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumReversedNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNumbs = Console.ReadLine().Split(' ')
+                .Select(int.Parse)
+                .ToList();
+
+            int sum = 0;
+
+            for (int i = 0; i < inputNumbs.Count; i++)
+            {
+                int currentNumber = inputNumbs[i];
+                int result = 0;
+                while (currentNumber > 0)
+                {
+                    int temp = currentNumber % 10;
+                    result = result * 10 + temp;
+                    currentNumber = currentNumber / 10;
+                }
+                sum += result;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
+
