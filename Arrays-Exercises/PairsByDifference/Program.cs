133ee6487651a628900ee0e226e84711a4ce463c
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 14:58:17 +0200

    PairsByDifference

---
+++ Arrays-Exercises/PairsByDifference/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairsByDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+            int difference = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    if (inputArray[i]-inputArray[j]==difference)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
