c6efe3195a6c3b52e1b7772fb0dab7fe93b1ea48
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 18:16:14 +0200

    RemoveNegativesAndReverse

---
+++ Lists - Lab/RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNumbs = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            for (int i = 0; i < inputNumbs.Count; i++)
+            {
+                if (inputNumbs[i] < 0)
+                {
+                    inputNumbs.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            if (inputNumbs.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                inputNumbs.Reverse();
+                Console.WriteLine(string.Join(" ", inputNumbs));
+            }
+        }
+    }
+}
