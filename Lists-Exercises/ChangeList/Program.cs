21b5b84410f30b5e11e9730f5a2acc24cf5aec0b
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 18:19:51 +0200

    ChangeList

---
+++ Lists-Exercises/ChangeList/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNums = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            while (command != "Even" &&
+                command != "Odd")
+            {
+                var tokens = command
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                if (tokens[0] == "Delete")
+                {
+                    int numberToRemove = int.Parse(tokens[1]);
+                    inputNums.RemoveAll(n => n == numberToRemove);
+                }
+                else if (tokens[0] == "Insert")
+                {
+
+                    inputNums.Insert(int.Parse(tokens[2]), int.Parse(tokens[1]));
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            if (command == "Even")
+            {
+                for (int i = 0; i < inputNums.Count; i++)
+                {
+                    if (inputNums[i] % 2 == 1)
+                    {
+                        inputNums.RemoveAt(i);
+                        i--;
+                    }
+                }
+            }
+
+            else if (command == "Odd")
+            {
+                inputNums = inputNums.Where(n => n % 2 == 1).ToList();
+            }
+            Console.WriteLine(string.Join(" ", inputNums));
+        }
+    }
+}
