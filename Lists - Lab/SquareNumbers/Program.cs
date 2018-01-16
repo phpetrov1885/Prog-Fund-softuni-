a3e270d9dcef62e7f1cc2f874722863a0caf1bc5
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 18:17:30 +0200

    SquareNumbers

---
+++ Lists - Lab/SquareNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNums = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            List<int> result = new List<int>();
+
+
+            for (int i = 0; i < inputNums.Count; i++)
+            {
+                if (Math.Sqrt(inputNums[i])==(int)Math.Sqrt(inputNums[i]))
+                {
+                    result.Add(inputNums[i]);
+                }
+              
+            }
+            result.Sort((a,b)=>b.CompareTo(a));
+            Console.WriteLine(string.Join(" ",result));
+        }
+    }
+}
