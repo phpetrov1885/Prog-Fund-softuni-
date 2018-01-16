95e2511edad49d8f96cec9f8d94f606c2c981ec9
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-16 18:16:40 +0200

    SortNumbers

---
+++ Lists - Lab/SortNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> inputNumbers = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToList();
+
+            inputNumbers.Sort();
+
+            Console.WriteLine(string.Join(" <= ",inputNumbers));
+        }
+    }
+}
