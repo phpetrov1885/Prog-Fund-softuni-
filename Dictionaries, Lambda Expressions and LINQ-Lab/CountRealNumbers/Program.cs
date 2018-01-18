c7300594a2a69a578001592fb17a518a6a2afd8e
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-18 13:13:52 +0200

    CountRealNumbers

---
+++ Dictionaries, Lambda Expressions and LINQ-Lab/CountRealNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double,int> numbers = new SortedDictionary<double, int>();
+
+            double[] inputArr = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse).ToArray();
+
+            foreach (double num in inputArr)
+            {
+                if (numbers.ContainsKey(num))
+                {
+                    numbers[num]++;
+                }
+                else
+                {
+                    numbers[num] = 1;
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
