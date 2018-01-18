9a26eccf4299e1fa5304ac1f55925f874f1284b4
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-18 13:14:28 +0200

    FoldAndSum

---
+++ Dictionaries, Lambda Expressions and LINQ-Lab/FoldAndSum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoldAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArr = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int k = inputArr.Length / 4;
+
+            int[] leftRow = inputArr.Take(k).Reverse().ToArray();
+            int[] middleRow = inputArr.Skip(k).Take(2 * k).ToArray();
+            int[] rightRow = inputArr.Skip(leftRow.Length + middleRow.Length).Reverse().ToArray();
+
+            int [] resultArr = new int[k*2];
+
+            for (int i = 0; i < resultArr.Length-k; i++)
+            {
+                resultArr[i] = middleRow[i] + leftRow[i];
+                resultArr[i+k] = middleRow[i+k] + rightRow[i];
+            }
+
+            Console.WriteLine(String.Join(" ", resultArr));
+           
+        }
+    }
+}
