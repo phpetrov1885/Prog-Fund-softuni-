43bbf5b66cd155a7326b55cb8668528563b3fb22
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-18 14:51:14 +0200

    Phonebook

---
+++ Dictionaries, Lambda Expressions and LINQ-Exercises/Phonebook/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+	    string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] command = input.Split(' ');
+
+                string name = command[1];
+
+	        if (command[0] == "A")
+                {
+                    string phoneNumber = command[2];
+                    phonebook[name] = phoneNumber;
+                }
+                else
+                {
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phonebook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
