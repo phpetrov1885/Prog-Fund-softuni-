c2d273bd460e793709dc919c67070f06674b2417
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-18 14:50:51 +0200

    MinerTask

---
+++ Dictionaries, Lambda Expressions and LINQ-Exercises/MinerTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string,int> resourcesList =new Dictionary<string, int>();
+
+            
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if (resource=="stop")
+                {
+                    break;
+                }
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resourcesList.ContainsKey(resource))
+                {
+                   resourcesList.Add(resource,quantity);
+                }
+
+                else
+                {
+                    resourcesList[resource] += quantity;
+                }
+           
+            }
+
+            foreach (var material in resourcesList)
+            {
+                Console.WriteLine($"{material.Key} -> {material.Value}");
+            }
+        }
+    }
+}
