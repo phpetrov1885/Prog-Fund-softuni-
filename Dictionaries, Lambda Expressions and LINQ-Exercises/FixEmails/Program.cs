db2d1bdedff512e100d6ca76320b3272dd6b11e6
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-18 14:49:31 +0200

    FixEmails

---
+++ Dictionaries, Lambda Expressions and LINQ-Exercises/FixEmails/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> emailDic = new Dictionary<string, string>();
+
+            string name = Console.ReadLine();
+
+            while (name != "stop")
+            {
+                string email = Console.ReadLine();
+
+                if (!emailDic.ContainsKey(name))
+                {
+                    emailDic.Add(name, email);
+                }
+                name = Console.ReadLine();
+            }
+
+            foreach (var user in emailDic)
+            {
+                if (!(user.Value.EndsWith("us") || user.Value.EndsWith("uk")))
+                {
+                    Console.WriteLine($"{user.Key} -> {user.Value} ");
+                }
+            }
+        }
+    }
+}
