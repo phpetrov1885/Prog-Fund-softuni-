388ae9435e53129776680783bb39d5800f4771f4
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-18 13:16:31 +0200

    SumMinMaxAverage

---
+++ Dictionaries, Lambda Expressions and LINQ-Lab/SumMinMaxAverage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumMinMaxAverage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countNumbers = int.Parse(Console.ReadLine());
+            int [] numbers = new int[countNumbers];
+
+
+            for (int i = 0; i < countNumbers; i++)
+            {
+               numbers[i]= int.Parse(Console.ReadLine());
+               
+            }
+
+            Console.WriteLine("Sum = " + numbers.Sum());
+            Console.WriteLine("Min = " + numbers.Min());
+            Console.WriteLine("Max = " + numbers.Max());
+            Console.WriteLine("Average = " + numbers.Average());
+        }
+    }
+}
