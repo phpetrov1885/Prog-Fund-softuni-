e0f1a97530f71841bee46c52ecb0336d27d84282
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-18 13:14:55 +0200

    LargestThreeNumbers

---
+++ Dictionaries, Lambda Expressions and LINQ-Lab/LargestThreeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNumbers = Console.ReadLine()
+                .Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            inputNumbers = inputNumbers.OrderByDescending(s => s).Take(3).ToList();
+
+            Console.WriteLine(string.Join(" ",inputNumbers));
+        }
+    }
+}
