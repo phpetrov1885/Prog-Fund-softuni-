a1db834f568710a6de34656bee7a7f05a1c1bd41
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-18 13:16:01 +0200

    ShortWordsSorted

---
+++ Dictionaries, Lambda Expressions and LINQ-Lab/ShortWordsSorted/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortWordsSorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine()
+                .ToLower()
+                .Split(new char[] {'.', ',', ':', ';', '(', ')', '[', ']', '\\', '\"', '\'', '/', '!', '?', ' '},
+                    StringSplitOptions.RemoveEmptyEntries);
+
+            var result = input.Where(s => s.Length < 5).OrderBy(s => s).Distinct();
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
