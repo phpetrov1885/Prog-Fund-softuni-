2d1a88b09b9d1ea2bf255146939a6553d97d241b
Unforgiven25 <31486310+Unforgiven25@users.noreply.github.com>
2018-01-18 13:15:24 +0200

    OddOccurrences

---
+++ Dictionaries, Lambda Expressions and LINQ-Lab/OddOccurrences/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<string,int> words = new Dictionary<string, int>();
+
+            string[] wordInput = Console.ReadLine().ToLower().Split(' ');
+
+            foreach (var count in wordInput)
+            {
+                if (words.ContainsKey(count))
+                {
+                    words[count]++;
+                }
+                else
+                {
+                    words[count] = 1;
+                }
+            }
+
+            List<string> result = new List<string>();
+
+            foreach (var word in words)
+            {
+                int count = word.Value;
+                
+                if (count % 2 != 0)
+                {
+                   result.Add(word.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ",result));
+        }
+    }
+}
